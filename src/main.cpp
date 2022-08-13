c4a8c482c4b3cfa73002e1837e8c275fd14c1b50
Kory Draughn <korydraughn@renci.org>
2022-08-13 18:30:00 -0400

    testing tabs and spaces and max line width

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,14 @@
 
 #include <fmt/format.h>
 
-int func_with_many_params(int a, int b, int c, int d, int aa, int bb, int cc, int dd)
+int func_with_many_params(int a,
+                          int b,
+                          int c,
+                          int d,
+                          int aa,
+                          int bb,
+                          int cc,
+                          int dd)
 {
 	return 0;
 }
@@ -12,32 +19,38 @@ int func_with_many_params(int a, int b, int c, int d, int aa, int bb, int cc, in
 int main()
 {
 	int long_variable_name_for_function_1 = 0;
-	func_with_many_params(long_variable_name_for_function_1,
-	                      long_variable_name_for_function_1,
-	                      long_variable_name_for_function_1,
-	                      long_variable_name_for_function_1,
-	                      long_variable_name_for_function_1,
-	                      long_variable_name_for_function_1,
-	                      long_variable_name_for_function_1,
-	                      func_with_many_params(long_variable_name_for_function_1,
-	                                            long_variable_name_for_function_1,
-	                                            long_variable_name_for_function_1,
-	                                            long_variable_name_for_function_1,
-	                                            long_variable_name_for_function_1,
-	                                            long_variable_name_for_function_1,
-	                                            long_variable_name_for_function_1,
-	                                            long_variable_name_for_function_1));
+	func_with_many_params(
+		long_variable_name_for_function_1,
+		long_variable_name_for_function_1,
+		long_variable_name_for_function_1,
+		long_variable_name_for_function_1,
+		long_variable_name_for_function_1,
+		long_variable_name_for_function_1,
+		long_variable_name_for_function_1,
+		func_with_many_params(long_variable_name_for_function_1,
+	                          long_variable_name_for_function_1,
+	                          long_variable_name_for_function_1,
+	                          long_variable_name_for_function_1,
+	                          long_variable_name_for_function_1,
+	                          long_variable_name_for_function_1,
+	                          long_variable_name_for_function_1,
+	                          long_variable_name_for_function_1));
 
 	rodsEnv env;
 
 	if (const int ec = getRodsEnv(&env); ec != 0) {
-		// Failed to load the local environment information (i.e. irods_environment.json).
-		// Handle error.
+		// Failed to load the local environment information (i.e.
+		// irods_environment.json). Handle error.
 		return 1;
 	}
 
 	rErrMsg_t error;
-	RcComm* conn = rcConnect(env.rodsHost, env.rodsPort, env.rodsUserName, env.rodsZone, 0, &error);
+	RcComm* conn = rcConnect(env.rodsHost,
+	                         env.rodsPort,
+	                         env.rodsUserName,
+	                         env.rodsZone,
+	                         0,
+	                         &error);
 
 	if (!conn) {
 		// Failed to connect to server.
@@ -60,7 +73,8 @@ int main()
 	// Use the "conn" pointer to execute operations.
 	//
 
-	SpecificQueryInp input{}; // Curly braces are equivalent to using std::memset to clear the object.
+	SpecificQueryInp input{}; // Curly braces are equivalent to using
+	                          // std::memset to clear the object.
 
 	char query_alias[] = "ShowCollAcls";
 	input.sql = query_alias;
@@ -69,12 +83,14 @@ int main()
 	input.args[0] = input_arg;
 
 	// Fetch the maximum number of rows for a single page.
-	// This is specific to iRODS. The maximum page size has nothing to do with the database.
+	// This is specific to iRODS. The maximum page size has nothing to do with
+	// the database.
 	input.maxRows = MAX_SQL_ROWS;
 
-	// So, we've set up our query. Now, we need to execute it. To do that, "rcGenQuery" requires that
-	// we pass it a pointer. The pointer we give to "rcGenQuery" will point to memory holding the
-	// results of our query. Following execution of the query, if nothing goes wrong, we will be able
+	// So, we've set up our query. Now, we need to execute it. To do that,
+	// "rcGenQuery" requires that we pass it a pointer. The pointer we give to
+	// "rcGenQuery" will point to memory holding the results of our query.
+	// Following execution of the query, if nothing goes wrong, we will be able
 	// to use the pointer to iterate over the results. More on that later.
 	GenQueryOut* output{};
 
@@ -92,15 +108,16 @@ int main()
 		}
 
 		//
-		// At this point, we know "output" contains the information we're interested in.
-		// All we have to do is print it.
+		// At this point, we know "output" contains the information we're
+		// interested in. All we have to do is print it.
 		//
 
 		// Iterate over each row.
 		for (int row = 0; row < output->rowCnt; ++row) {
 			fmt::print("row: ");
 
-			// Iterate over each attribute (i.e. this is a column or the result of an aggregate function).
+			// Iterate over each attribute (i.e. this is a column or the result
+			// of an aggregate function).
 			for (int attr = 0; attr < output->attriCnt; ++attr) {
 				const SqlResult* sql_result = &output->sqlResult[attr];
 				const char* value = sql_result->value + (row * sql_result->len);
@@ -115,9 +132,9 @@ int main()
 			break;
 		}
 
-		// To move to the next page of the resultset, copy the continue index contained in the output
-		// structure to the continue index of the input structure. Forgetting to do this will result in
-		// an infinite loop!
+		// To move to the next page of the resultset, copy the continue index
+		// contained in the output structure to the continue index of the input
+		// structure. Forgetting to do this will result in an infinite loop!
 		input.continueInx = output->continueInx;
 
 		clearGenQueryOut(output);
@@ -126,8 +143,8 @@ int main()
 	clearGenQueryInp(&input);
 	clearGenQueryOut(output);
 
-	// When done, call rcDisconnect() so that the server knows the client is finished.
-	// This lets the server know it is okay to deallocate resources.
+	// When done, call rcDisconnect() so that the server knows the client is
+	// finished. This lets the server know it is okay to deallocate resources.
 	rcDisconnect(conn);
 
 	return 0;
